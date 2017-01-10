5cfb9c95b1731860309a33104f75c66cee1117f6
Jason Summers <jwsummers.dev@gmail.com>
2017-01-09 21:20:55 -0500

    Update javascript.js
    Was able to add tweet quote option.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-var quotes = [
+var currentQuote = '';
+var availableQuotes = [
   'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. -Thomas A. Edison',
   'Our greatest glory is not in never falling, but in rising every time we fall. -Confucius',
   'In the middle of every difficulty lies opportunity. -Albert Einstein',
@@ -8,9 +9,17 @@ var quotes = [
   'In three words I can sum up everything I\’ve learned about life: it goes on.-Robert Frost',
   'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that. -Martin Luther King Jr.',
   'The fear of death follows from the fear of life. A man who lives fully is prepared to die at any time. -Mark Twain'
-]
+];
 
-function newQuote() {
-  var randomNumber = Math.floor(Math.random() * (quotes.length));
-  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+function setRandomQuote() {
+	currentQuote = availableQuotes[Math.floor(Math.random()*availableQuotes.length)];
+	
+	$('#quote').html(currentQuote);
+	$('#tweetQuote').attr('href', 'https://twitter.com/intent/tweet?text='+currentQuote).attr('target', '_blank');
 }
+
+$(function() {
+	$('#randomQuote').click(function() {
+		setRandomQuote();
+	});
+});
